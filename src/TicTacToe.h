f3a9d87c344603f4fa54a5ca157009b2e743b280
roshan-maradana <bs21b019@smail.iitm.ac.in>
2024-06-20 19:08:32 +0530

    Uploading files

---
+++ src/TicTacToe.h
@@ -0,0 +1,14 @@
+#ifndef TICTACTOE_H_
+#define TICTACTOE_H_
+
+void init();
+bool validate();
+void clearScreen();
+void showBoard();
+bool gameOver(); //implement this method
+
+
+
+
+
+#endif /* TICTACTOE_H_ */
